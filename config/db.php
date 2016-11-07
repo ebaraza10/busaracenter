e601040e716392fce80b28a4d9b50ba80da4b2f0
ebaraza10 <ebaraza10@gmail.com>
2016-11-07 07:28:43 +0300

    Update db.php

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=busaracenter',
-    'username' => 'root',
-    'password' => 'EliBaz2010',
+    'username' => 'Database usernae',
+    'password' => 'database password',
     'charset' => 'utf8',
 ];
